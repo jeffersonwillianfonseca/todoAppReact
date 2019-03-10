8aca304afe4d9472d9689b19365f832c43c50151
JeffersonWillian <jefferson.has.fonseca@gmail.com>
2019-03-10 10:00:49 -0300

    Adiciona projeto do backend

---
+++ backend/src/config/cors.js
@@ -0,0 +1,6 @@
+module.exports = function(re, res, next) {
+    res.header('Access-COntrol-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+}
---
+++ backend/src/config/database.js
@@ -0,0 +1,3 @@
+const mongoose = require = require('mongoose')
+mongoose.Promise = global.Promise
+module.exports = mongoose.connect('mongodb://localhost/todo')
