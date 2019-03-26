a045e71d6c86dfc9d64c41aaca07df8d5312b5b8
JeffersonWillian <jefferson.has.fonseca@gmail.com>
2019-03-25 21:36:38 -0300

    Adicionar conclusao do projeto

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,16 +2,37 @@ import React from 'react'
 import Grid from '../template/grid'
 import IcconButton from '../template/icconButton'
 
-export default props => (
-    <div role='form' className='todoForm'>
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key == 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
 
-        <Grid cols='12 9 10'>
-            <input id='description' className='form-control'
-                placeholder='Adicione uma tarefa'></input>
-        </Grid>
+    return (
+        <div role='form' className='todoForm'>
 
-        <Grid cols='12 3 2'>
-            <IcconButton style='primary' icon='plus'></IcconButton>
-        </Grid>
-    </div>
-)
+            <Grid cols='12 9 10'>
+                <input id='description' className='form-control'
+                    placeholder='Adicione uma tarefa'
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}>
+                </input>
+            </Grid>
+
+            <Grid cols='12 3 2'>
+                <IcconButton style='primary' icon='plus'
+                    onClick={props.handleAdd}>
+                </IcconButton>
+                <IcconButton style='info' icon='search'
+                    onClick={props.handleSearch}>
+                </IcconButton>
+                <IcconButton style='default' icon='close'
+                    onClick={props.handleClear}>
+                </IcconButton>
+            </Grid>
+        </div>)
+}
+
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import IcconButton from '../template/icconButton'
 
-export default props => (
-    <div>
-        <h1>
-            TodoList
-        </h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td className={todo.done ? 'markedAsDone' : ''}>
+                    {todo.description}
+                </td>
+                <td>
+                    <IcconButton style='success' icon='check' hide={todo.done}
+                        onClick={() => props.changeToDone(todo)}>
+                    </IcconButton>
+                    <IcconButton style='warning' icon='reply' hide={!todo.done}
+                        onClick={() => props.changeToPending(todo)}>
+                    </IcconButton>
+                    <IcconButton style='danger' icon='trash-o' hide={!todo.done}
+                        onClick={() => props.handleRemove(todo)}>
+                    </IcconButton>
+                </td>
+            </tr>
+        ))
+
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th className='tableActions'>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
